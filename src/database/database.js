5949b8b2eb95d292551b5cd674231b408bbf8558
Renard Bergson <renardrock@gmail.com>
2023-10-22 14:19:25 -0300

    inserindo database remoto

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 function connect () {
-    mongoose.connect('mongodb://localhost:27017/pedidos-de-lanchonete')
+    // remote connection - mongodb atlas
+    const user = 'admin'
+    const pass = 'admin'
+    const database = 'pedidos-de-lanchonete'
+
+    mongoose.connect(`mongodb+srv://${user}:${pass}@pedidos-de-lanchonete.ya6oudc.mongodb.net/${database}?retryWrites=true&w=majority`)
+    
     const db = mongoose.connection
+    
     db.once('open', () => {console.log('application connected to database')})
     db.on('error', () => {console.error.bind(console, 'some error has ocurred!')})
 }
